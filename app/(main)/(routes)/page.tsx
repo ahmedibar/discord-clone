1b350510989ae46dfd39f6c4980e820822fec887
Ahmed Nur Ali <88971150+ahmedibar@users.noreply.github.com>
2024-05-27 19:53:20 +0530

    Authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold text-indigo-500">
+        <UserButton />
         This is a protected route.
       </p>
     </div>
