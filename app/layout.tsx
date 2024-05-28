b7e81aee69a3364ecad558877b6be43d91184cbf
Ahmed Nur Ali <88971150+ahmedibar@users.noreply.github.com>
2024-05-28 11:55:24 +0530

    Prisma and Database Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
             storageKey="discord-theme"
+            forcedTheme="dark"
             enableSystem={false}
             disableTransitionOnChange
           >
